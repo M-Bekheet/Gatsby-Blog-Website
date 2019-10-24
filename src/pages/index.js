0e357970c5fa0c16a50ed07e8cd872982cc30cd9
Lartwel <mlartwel@gmail.com>
2019-10-24 15:39:36 +0200

    Added Carousel Sample to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,28 @@
 import React from "react"
 import Head from '../components/head';
-import Layout from '../components/layout';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
+import Layout from '../components/layout';
 const IndexPage = () => (
   <Layout>
     <Head title="Home"/>
     <h1>Hello</h1>
     <h2>This is my home page.</h2>
+     <Carousel>
+        <div>
+        <img alt="slide1" src="White-Clouds.jpg" />
+            <p className="legend">White Clouds</p>
+        </div>
+        <div>
+        <img alt="slide2" src="Winter-1.jpg" />
+            <p className="legend">Winter</p>
+        </div>
+        <div>
+        <img alt="slide3" src="Woods-1.jpg" />
+            <p className="legend">Woods</p>
+        </div>
+    </Carousel>
   </Layout>
 )
 
